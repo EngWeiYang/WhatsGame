5b0c95fe2776e1671ac0cfdc093a90c3a50fc92a
Lychesann <zukhairyfaris2003@gmail.com>
2024-07-04 19:44:11 +0800

    Added Level 2 for prototype

---
+++ Assets/Script/ReplyingMessage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReplyingMessage : MonoBehaviour
+{
+    public InputField messageInputField;
+    public Button sendButton;
+    public Text chatDisplay;
+    public GameObject winScreen;
+
+    void Start()
+    {
+        sendButton.onClick.AddListener(SendMessage);
+        winScreen.SetActive(false);
+    }
+
+    void SendMessage()
+    {
+        string message = messageInputField.text;
+
+        if (!string.IsNullOrEmpty(message))
+        {
+            // Add the message to the chat display
+            chatDisplay.text += message;
+
+            // Clear the input field
+            messageInputField.text = "";
+        }
+        StartCoroutine(WinScreen());
+    }
+    IEnumerator WinScreen()
+    {
+        yield return new WaitForSeconds(1);
+        winScreen.SetActive(true);
+    }
+}
