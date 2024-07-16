38616613345ae3deb546f63087fda9992ad1299b
Faris <105273863+Lychesann@users.noreply.github.com>
2024-07-16 16:11:44 +0800

    Level 5 not yet done but skeleton

--- Assets/Script/VoiceMessageLevel.cs
+++ Assets/Script/VoiceMessageLevel.cs
@@ -4,30 +4,31 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+//Logic flow
+//Player will start at the Chat page
+//Player will need to press and hold the record button. Firefly will explain the other button shown
+//Player can let go after firefly explain finish
+//Level Finish 🎉
 
-public class VoiceMessageLevel : MonoBehaviour
+
+//Code flow
+//Count up timer to show in input box
+//Alert pop up when player let go
+//Dynamic chat bubble?
+//Do a coroutine when button is clicked text show up and recording timer increases
+//OnDrag to give the voice recording
+public class VoiceMessageLevel : MonoBehaviour, IPointerDownHandler
 {
-    //Logic flow
-    //Player will start at the Chat page
-    //Player will need to press and hold the record button. Firefly will explain the other button shown
-    //Player can let go after firefly explain finish
-    //Level Finish 🎉
-
-
-    //Code flow
-    //Count up timer to show in input box
-    //Alert pop up when player let go
-    //Dynamic chat bubble?
-
-
     public GameObject recordButton;
     public GameObject currentlyRecordingButton;
-    public TMP_Text timer;
-    public GameObject firefly;
-    public TMP_Text fireflyExplanation;
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        recordButton.SetActive(false);
+        currentlyRecordingButton.SetActive(true);
+    }
 
-    
 }
