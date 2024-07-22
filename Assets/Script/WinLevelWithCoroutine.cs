af60978419961d34b9a8654bff2b315996e6c9d8
Faris <105273863+Lychesann@users.noreply.github.com>
2024-07-22 16:37:08 +0800

    made couroutine manager

--- Assets/Script/WinLevelWithCoroutine.cs
+++ Assets/Script/WinLevelWithCoroutine.cs
@@ -8,18 +8,27 @@ public class WinLevelWithCoroutine : MonoBehaviour
     public GameObject levelCompleteScreen;
     public Animator levelCompleteAnimator;
     public Button levelCompleteEnable;
+    private CoroutineManager coroutineManager;
     // Start is called before the first frame update
     private void Start()
     {
         // Subscribe to the button's onClick event
         levelCompleteEnable.onClick.AddListener(EnableLevelCompletion);
+        GameObject coroutineManagerObject = GameObject.Find("CoroutineManager");
+        coroutineManager = coroutineManagerObject.GetComponent<CoroutineManager>();
     }
 
     void EnableLevelCompletion()
     {
-        // Enable the level complete screen and trigger the animation
-
+        // Enable the level complete screen and trigger the animatio
+        coroutineManager.StartManagedCoroutine(WinScreenCoroutine());
+    }
+    IEnumerator WinScreenCoroutine()
+    {
+        // Wait for the animation to finish (assumes the animation length is 2 seconds)
+        yield return new WaitForSeconds(2.0f);
         levelCompleteScreen.SetActive(true);
-        levelCompleteAnimator.SetTrigger("LevelComplete");
+        levelCompleteAnimator.SetTrigger("LevelCompleted");
+        
     }
 }
