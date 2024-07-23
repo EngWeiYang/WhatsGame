bbc85aafde265d8aced20b37dde423000349f79b
Faris <105273863+Lychesann@users.noreply.github.com>
2024-07-23 16:52:29 +0800

    Crazy stuff happened for level 8 still need to do other things for it disable and enable gameobjects

---
+++ Assets/Script/CallingWithCoroutine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CallingWithCoroutine : MonoBehaviour
+{
+    public GameObject screenRinging;
+    public GameObject screenAnswer;
+    public Button callButton;
+    private CoroutineManager coroutineManager;
+    void Start()
+    {
+        callButton.onClick.AddListener(CallingandAnswering);
+        GameObject coroutineManagerObject = GameObject.Find("CoroutineManager");
+        coroutineManager = coroutineManagerObject.GetComponent<CoroutineManager>();
+    }
+
+    // Update is called once per frame
+    void CallingandAnswering()
+    {
+        screenRinging.SetActive(true);
+        coroutineManager.StartManagedCoroutine(FromRingingToAnswering());
+    }
+    IEnumerator FromRingingToAnswering()
+    {
+        // Wait for the animation to finish (ADD IN AUDIO LATER)
+        yield return new WaitForSeconds(5f);
+        screenRinging.SetActive(false);
+        screenAnswer.SetActive(true);
+        
+
+    }
+}
